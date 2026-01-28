4b6b4bd8556f0d813cfe88dac9660126b5e42fdb
Omar Shafik <omarshafikcollege@gmail.com>
2026-01-28 08:38:44 -0500

    Completed Logic & Tested

--- tucp.c
+++ tucp.c
@@ -19,5 +19,82 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    //For loop t oprocess files
+    for (int i = 1; i < argc - 1; i++) {
+        char final_dest[100];
+
+        //Path Constructor
+        if (is_dir == 1) {
+            strcpy(final_dest, argv[argc-1]);
+            strcat(final_dest, "/");
+            strcat(final_dest, argv[i]);
+        } else {
+            //if not a directory then just use name directly
+            strcpy(final_dest, argv[argc-1]);
+        }
+
+        // reads current file with guard
+        FILE *src = fopen(argv[i], "rb");
+        if (src == NULL) {
+            printf("Error: Cannot open source file %s\n", argv[i]);
+            continue; //skips current loop iteration
+        }
+
+        // writes to dest with guard 
+        FILE *dst = fopen(final_dest, "wb");
+        if (dst == NULL) {
+            printf("Error: Cannot create destination\n");
+            fclose(src); // closes source file in case of error
+            continue;
+        }
+
+        // copies byte by byte
+        int ch;
+        while ((ch = fgetc(src)) != EOF) {
+            fputc(ch, dst);
+        }
+
+        //ensuring files close before end of program
+        fclose(src);
+        fclose(dst);
+    }
+
     return 0;
 }
+
+//Test cases
+    // gcc tucp.c -o tucp -Wall -Werror
+
+//Case 1: File to File (test.txt to test2.txt)
+    // ./tucp test.txt test2.txt
+    // cat test2.txt
+
+//Case 2: Image Copy (ImageToCopy.png to ImageToCopy2.png)
+    // ./tucp ImageToCopy.png ImageToCopy2.png
+
+
+//Case 3: Overwrite test2.txt with second_test.txt
+    // ./tucp test_second.txt test2.txt
+    // cat test2.txt
+
+//Case 4: Nonexistent file (Should print an error)
+    // ./tucp non_existent_file.txt nowhere.txt
+
+//Case 5: File to Directory
+    // mkdir test
+    // ./tucp test.txt test
+    // ls test
+
+//Case 6: Multiple files to Directory
+    // ./tucp test.txt ImageToCopy.png test
+    // ls test
+
+//Case 7: Copy to nonexistent directory (Should print an error)
+    // ./tucp test.txt folder_that_doesnt_exist/
+
+//Case 8: One argument error (Should print an error)
+    // ./tucp test.txt
+
+//Case 9: Two directories error (Should print an error)
+    // mkdir test_dir1 test_dir2
+    // ./tucp test_dir1 test_dir2
--- tuls.c
+++ tuls.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
     char *target_dir;
@@ -23,5 +24,51 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    //Read items one by one
+    struct dirent *entry;
+    while ((entry = readdir(folder)) != NULL) {
+        
+        // skipping hidden files
+        if (entry->d_name[0] == '.') {
+            continue;
+        }
+
+        //path constructor for stat
+        struct stat st;
+        char path_to_check[600];
+        strcpy(path_to_check, target_dir);
+        strcat(path_to_check, "/");
+        strcat(path_to_check, entry->d_name);
+
+        //determining wheter it is a directory or file to print.
+        if (stat(path_to_check, &st) == 0 && S_ISDIR(st.st_mode)) {
+            printf("\033[38;5;208m%s\033[0m\n", entry->d_name); // entry -> d_name retrieves directory or file name
+        } else {
+            printf("%s\n", entry->d_name);
+        }
+    }
+
+    //Clean up
+    closedir(folder);
+
     return 0;
 }
+
+//Test Cases
+    // gcc tuls.c -o tuls -Wall -Werror
+
+// Case 1: No arguments (Lists current directory)
+    // ./tuls
+
+// Case 2: Specific directory path
+    // ./tuls test_dir_1
+
+// Case 3: Too many arguments (Should print an error)
+    // ./tuls test test2
+
+// Case 4: Hidden directory check (Should NOT show .git or other dots)
+    // ls -a    # This shows everything, including hidden ones
+    // ./tuls   # Run this to prove your code filters the dots out
+
+// Case 5: Nonexistent directory (Should print "tuls: cannot open directory")
+    // ./tuls folder_made_up
